48c4da839727598a091a2082094c05dde09038d8
EC2 Default User <ec2-user@ip-172-31-47-118.ap-northeast-2.compute.internal>
2022-11-12 13:59:37 +0900

    배포 스크립트 수정

--- deploy.sh
+++ deploy.sh
@@ -3,24 +3,31 @@
 REPOSITORY=/home/ec2-user
 PROJECT_NAME=CoTEVer-Server
 
+rm ../nohup.out
+
+sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-ports 8080
+
 cd $REPOSITORY/$PROJECT_NAME/
 
 echo "> Git Pull"
 
 git pull
 
-./gradlew build
+gradle build -x test
 
 cd $REPOSITORY
 
-cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
+cp $REPOSITORY/$PROJECT_NAME/build/libs/CoTEVer-0.0.1-SNAPSHOT.jar $REPOSITORY/
 
-CURRENT_PID = $(pgrep -f ${PROJECT_NAME}.*.jar)
+CURRENT_PID=$(pgrep -f CoTEVer-0.0.1-SNAPSHOT.jar)
+
+echo " 현재 구동중인 애플리케이션pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
 	echo "> 현재 구동중인 서버가 없음"
 else
 	kill -15 $CURRENT_PID
+	echo "> kill"
 	sleep 5
 fi
 
